15997e82a67b4b51a3fec7400c2d02dbb975d958
ckrishi <ckrishi@gmail.com>
2017-10-08 22:30:54 -0700

    Initial change

--- malayalamcinemacrawler/Program.cs
+++ malayalamcinemacrawler/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace malayalamcinemacrawler
 {
+    class Crawl
+    {
+        public async System.Threading.Tasks.Task<string> GetStreamAsync(string url)
+        {
+            WebRequest wr = WebRequest.Create(url);
+            wr.Method = "GET";
+            System.Threading.Tasks.Task<WebResponse> response = wr.GetResponseAsync();
+            WebResponse rr = await response;
+            System.IO.Stream dataStream  = rr.GetResponseStream();
+           
+            StreamReader streamReader = new StreamReader(dataStream);
+            string html = streamReader.ReadToEnd();
+            System.Console.WriteLine(html);
+            return html;
+
+
+        }
+
+    }
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+
+            Crawl c = new Crawl();
+            Task t = c.GetStreamAsync("http://www.malayalamcinema.com/meet-the-star.php");
+            t.ContinueWith(str
+                 => Console.WriteLine(str));
+            t.Wait();
+
+
+
+         //   string s = await c.GetStreamAsync("http://www.malayalamcinema.com/meet-the-star.php");
+
         }
+
+      
     }
 }
